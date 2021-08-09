a8dac6caf6ebfe3c8eb0e0e5a1ddeffdd6e6dc9c
khayyumkureshi <88646390+khayyumkureshi@users.noreply.github.com>
2021-08-09 11:44:47 +0530

    Delete train-case-study directory

--- train-case-study/src/main/java/com/spring/repository/TrainRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.model.Train;
-
-public interface TrainRepository extends JpaRepository<Train,Integer>{
-	
-}
